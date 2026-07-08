a25a2a74a595f3662d2073a477e95395331da6af
hemeshhere <hh9199939655@gmail.com>
2026-07-09 00:13:56 +0530

    completed the fourth handson code of 2.spring-rest-handson

---
+++ Week 3/Spring REST using Spring Boot 3/2. spring-rest-handson/rest/src/main/java/com/project/service/CountryService.java
@@ -0,0 +1,21 @@
+package com.project.service;
+
+import com.project.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> countries = context.getBean("countryList", List.class);
+        return countries.stream()
+                .filter(country -> country.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
+}
